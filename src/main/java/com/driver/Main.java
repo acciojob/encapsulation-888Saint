66d24f88b451e62cd826a754088c01777402bd0e
Saint <psdas32@gmail.com>
2024-10-16 19:21:29 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Create an object of RWOnly class
+        RWOnly obj = new RWOnly();
+
+        // Task 6: Set a value using the setter and access it using the getter
+        obj.setName("John");  // Using the setter to set the value of name
+        System.out.println(obj.getName());  // Using the getter to access the value of name
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
-package com.driver;
+package com.driver;  // assuming the package is named com.driver
 
 public class RWOnly {
-    
+    // Private member variable
+    private String name;
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
